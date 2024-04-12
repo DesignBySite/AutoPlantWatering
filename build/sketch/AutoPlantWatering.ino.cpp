18bdc0c03d2b1c7995fdad77c4bf53d01e57e61a
KevinHome <kevinhome@KevinHomes-MacBook-Pro.local>
2024-04-12 12:40:24 -0400

    connected to wifi

--- build/sketch/AutoPlantWatering.ino.cpp
+++ build/sketch/AutoPlantWatering.ino.cpp
@@ -1,56 +1,53 @@
 #include <Arduino.h>
 #line 1 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
 #include <Adafruit_ADS1X15.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include "./secrets.h"
+
+const char* ssid = SECRET_WIFI;
+const char* password = SECRET_PASS;
+const char* serverUrl = "http://yourserver.com/data";
 
 Adafruit_ADS1015 ads;
 
-#define D3_PIN D3 // Define the pin connected to the relay
-
-#line 7 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
+#line 12 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
 void setup();
-#line 13 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
+#line 29 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
 void loop();
-#line 37 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
+#line 36 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
 void engageWatering(int channel, int pinNum);
-#line 60 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
+#line 57 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
 void moisturePrint(int channel, int moisture);
-#line 7 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
+#line 12 "/Users/kevinhome/AutoPlantWatering/AutoPlantWatering.ino"
 void setup() {
   Serial.begin(9600); // Initialize serial communication
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  };
+  delay(1000);
+  Serial.print("Connected Successfully to ");
+  Serial.println(SECRET_WIFI);
+  
   ads.begin(0x48);
-  pinMode(D3_PIN, OUTPUT); // Set the D3 pin as an outputP
+  pinMode(D3, OUTPUT); // Set the D3 pin as an output
+  pinMode(D4, OUTPUT); // Set the D4 pin as an output
 }
 
 void loop() {
-  // Call the engageWatering function only when a certain condition is met.
-  // For example, you could use a button press, a sensor threshold, or just a simple delay as shown here.
+  // engageWatering(0, D3); // Call the watering function
+  // engageWatering(1, D4);
 
-
-  // printMoistureReading(0);
-  engageWatering(0, D3_PIN); // Call the watering function
-  engageWatering(1, D4);
-
-  delay(3000); // Wait for 30 seconds before calling the function again. Adjust as per your requirement.
+  delay(3000); // Wait for 3 seconds before calling the function again. Adjust as per your requirement.
 }
 
-// void printMoistureReading(int channel) {
-//   int16_t adc1;
-//   adc1 = ads.readADC_SingleEnded(channel);
-
-
-
-//   Serial.print("Moisture Level ADC Value pin 1: ");
-//   Serial.println(adc1);
-// }
-/************************************/
-/** Make this reusable for each pin */
-/************************************/
 void engageWatering(int channel, int pinNum) {
   int dryest = 938;
   int wettest = 448;
-
   int16_t adcValue = ads.readADC_SingleEnded(channel);
-
   int moisturePercentage = map(adcValue, wettest, dryest, 100, 0); //calculate moisture into a decimal
 
   moisturePrint(channel, moisturePercentage);
@@ -65,7 +62,7 @@ void engageWatering(int channel, int pinNum) {
         moisturePrint(channel, moisturePercentage);
       };
   }
-  digitalWrite(pinNum, HIGH);
+  digitalWrite(pinNum, HIGH); // disengage relay
 }
 
 void moisturePrint(int channel, int moisture) {
