6f008904c5e8cc3304c1aca84f0091f6ce777718
KevinHome <kevinhome@KevinHomes-MacBook-Pro.local>
2024-05-02 12:23:45 -0400

    added react and attempting eventSource, but need to finish soldering to send data from microcontroller

--- build/core/stdlib_noniso.cpp.d
+++
@@ -1,5 +0,0 @@
-/Users/kevinhome/AutoPlantWatering/build/core/stdlib_noniso.cpp.o: \
- /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/stdlib_noniso.cpp \
- /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/CommonHFile.h \
- /Users/kevinhome/AutoPlantWatering/build/core/AutoPlantWatering.ino.globals.h \
- /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/stdlib_noniso.h
