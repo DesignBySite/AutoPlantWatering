65e73eeff781be6751575995067f0d4c61acfc10
KevinHome <kevinhome@KevinHomes-MacBook-Pro.local>
2024-05-02 12:23:45 -0400

    added react and attempting eventSource, but need to finish soldering to send data from microcontroller

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const eventSource = new EventSource('http://localhost:3050/events');
+    eventSource.onmessage = (event) => {
+      const newMessage = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      {messages.map((msg, index) => (
+        <div key={index}>{msg.message}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
