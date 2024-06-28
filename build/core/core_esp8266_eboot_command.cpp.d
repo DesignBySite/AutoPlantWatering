e9d38a7e4763d7c49049961f6c43808d479feab3
KevinHome <kevinhome@KevinHomes-MacBook-Pro.local>
2024-06-27 20:21:27 -0400

    more clean up and refinement of cpp

---
+++ build/core/core_esp8266_eboot_command.cpp.d
@@ -0,0 +1,8 @@
+/Users/kevinhome/AutoPlantWatering/build/core/core_esp8266_eboot_command.cpp.o: \
+ /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/core_esp8266_eboot_command.cpp \
+ /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/CommonHFile.h \
+ /Users/kevinhome/AutoPlantWatering/build/core/AutoPlantWatering.ino.globals.h \
+ /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/coredecls.h \
+ /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/core_esp8266_features.h \
+ /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/cont.h \
+ /Users/kevinhome/Library/Arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/eboot_command.h
